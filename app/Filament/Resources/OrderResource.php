8772beb0a1afc38ef9b0840a9ee08cebd0687ee1
jon1267 <jonleev@gmail.com>
2025-07-16 08:52:04 +0300

    widget & relation manager

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -235,7 +235,7 @@ class OrderResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\AddressRelationManager::class,
         ];
     }
 
--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -16,4 +17,32 @@ class ListOrders extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderResource\Widgets\OrderStats::class,
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            null => Tab::make('All'),
+            'new' => Tab::make('New')->query(fn ($query) => $query->where('status', 'new')),
+            'processing' => Tab::make('Processing')->query(fn ($query) => $query->where('status', 'processing')),
+            'shipped' => Tab::make('Shipped')->query(fn ($query) => $query->where('status', 'shipped')),
+            'delivered' => Tab::make('Delivered')->query(fn ($query) => $query->where('status', 'delivered')),
+            'canceled' => Tab::make('Canceled')->query(fn ($query) => $query->where('status', 'canceled')),
+            // time: 00:01 Less 9 (8-E)
+        ];
+    }
+
+    /* или getHeaderWidgets() или этот
+     protected function getFooterWidgets(): array
+    {
+        return [
+            OrderResource\Widgets\OrderStats::class,
+        ];
+    }*/
 }
