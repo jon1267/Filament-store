c3d6e2374232682b9738db2e0ba8ede7fa8fe0e1
jon1267 <jonleev@gmail.com>
2025-07-21 13:38:17 +0300

    pass revealable && hide in edit

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -42,7 +42,9 @@ class UserResource extends Resource
                     Forms\Components\TextInput::make('password')
                         ->password()
                         ->required()
-                        ->maxLength(255),
+                        ->revealable()
+                        ->visibleOn('create'),
+
                     Forms\Components\DateTimePicker::make('email_verified_at'),
                     Forms\Components\FileUpload::make('avatar')
                         ->image()
